9ddc0473e0c4445a55430570c210fb10895a67b4
ImFineZ <greivinrodriguez@icloud.com>
2022-09-13 03:47:18 +0000

    AlmacenarEnElArray

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState("");
+	const [addTarea, setAddTarea] = useState([]);
+
+	function listaTareas(e) {
+		if (e.key === "Enter") {
+			setAddTarea([...addTarea, e.target.value]);
+			e.target.value = "";
+		}
+	}
+	console.log(addTarea);
+
+	listaTareas.pop
+
+	let tareaPendientes = "";
+	if (addTarea.length == 0) {
+		tareaPendientes = "No hay tarea pendientes "
+	} else {
+		tareaPendientes = "Tarea pendientes = ";
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classNameName="contenedor">
+			<h1 classNameName="text-center mt-5">Todos</h1>
+			<div classNameName="contenedor-tareas">
+				<div classNameName="input-group mb-3">
+					<input
+						type="type"
+						classNameName="form-control"
+						placeholder="Añadir Tarea"
+						aria-label="Recipient's username"
+						aria-describedby="basic-addon2"
+						onKeyDown={listaTareas}
+					/>
+
+				</div>
+				<div>
+					<div className="list-group">
+						<div>
+							<li className="list-group-item">Cras justo odio	</li>							
+						</div>						
+					</div>
+				</div>
+				<div>
+					<h4>{tareaPendientes}{addTarea.length}</h4>
+				</div>
+			</div>
 		</div>
+
 	);
 };
 
