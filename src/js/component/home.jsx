636b0d2d66c953577c61aef20b282aa7d9ab99c6
ImFineZ <greivinrodriguez@icloud.com>
2022-09-14 04:17:27 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,8 +4,10 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 
-	const [inputValue, setInputValue] = useState("");
+	//const [inputValue, setInputValue] = useState("");
 	const [addTarea, setAddTarea] = useState([]);
+
+
 
 	function listaTareas(e) {
 		if (e.key === "Enter") {
@@ -15,7 +17,11 @@ const Home = () => {
 	}
 	console.log(addTarea);
 
-	listaTareas.pop
+	const RemoveTodo = index => {
+		const newTodos = [...addTarea];
+		newTodos.splice(index, 1);
+		setAddTarea(newTodos);
+	};
 
 	let tareaPendientes = "";
 	if (addTarea.length == 0) {
@@ -25,29 +31,41 @@ const Home = () => {
 	}
 
 	return (
-		<div classNameName="contenedor">
-			<h1 classNameName="text-center mt-5">Todos</h1>
-			<div classNameName="contenedor-tareas">
-				<div classNameName="input-group mb-3">
+		<div className="contenedor">
+			<h1 className="text-center mt-5">To-do List</h1>
+			<div className="contenedor-tareas">
+				<div className="input-group mb-3">
 					<input
 						type="type"
-						classNameName="form-control"
+						className="form-control"
 						placeholder="Añadir Tarea"
 						aria-label="Recipient's username"
 						aria-describedby="basic-addon2"
 						onKeyDown={listaTareas}
 					/>
-
 				</div>
 				<div>
 					<div className="list-group">
 						<div>
-							<li className="list-group-item">Cras justo odio	</li>							
-						</div>						
+							<ul>
+								{addTarea.map((tarea, index) => (
+									<li key={index}>
+										{tarea}
+										<a
+											className="float-end"
+											onClick={() => RemoveTodo(index)}>
+											<i className="bi bi-x bg-primary text-white"></i>
+										</a>
+
+									</li>
+								))}
+							</ul>
+						</div>
 					</div>
 				</div>
 				<div>
-					<h4>{tareaPendientes}{addTarea.length}</h4>
+					<h4 className="text-center">{tareaPendientes}{addTarea.length}</h4>
+
 				</div>
 			</div>
 		</div>
